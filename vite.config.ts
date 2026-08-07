a353eafbeea62152e733dc63c692fc94d015c8a1
Emmanuel Drah <emmanueldrah@gmail.com>
2026-08-07 00:12:58 +0000

    new commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
   build: {
-    outDir: 'dist',
-    minify: 'esbuild',
+    outDir: "dist",
+    minify: "esbuild",
     sourcemap: false,
     rollupOptions: {
       input: {
-        home: 'index.html',
-        suites: 'rooms.html',
-        amenities: 'amenities.html',
-        gallery: 'gallery.html',
-        pricing: 'pricing.html',
-        contact: 'contact.html'
-      }
-    }
-  }
+        home: "index.html",
+        suites: "rooms.html",
+        amenities: "amenities.html",
+        gallery: "gallery.html",
+        pricing: "pricing.html",
+        contact: "contact.html",
+      },
+    },
+  },
 });
